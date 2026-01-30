45f362dcf02492a40a033ce66bf83e045c8ec748
pashi44 <pashireddi@gmail.com>
2026-01-30 12:31:41 -0500

    commit

---
+++ patientservice/src/main/java/com/pashi44/patientservice/PatientserviceApplication.java
@@ -0,0 +1,14 @@
+package com.pashi44.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class PatientserviceApplication {
+
+	public static void main(String[] args) {
+   ConfigurableApplicationContext psmain = SpringApplication.run(PatientserviceApplication.class, args);
+	}
+
+}
