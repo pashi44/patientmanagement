3f3423f2a476d6b606b75d94f3accb417fe40a9b
pashi44 <pashireddi@gmail.com>
2026-02-09 20:26:00 -0500

    stopped at billing serive clinet in patient

--- patientservice/src/main/java/com/pashi44/patientservice/grpc/BillingServiceGrpcClient.java
+++ patientservice/src/main/java/com/pashi44/patientservice/grpc/BillingServiceGrpcClient.java
@@ -1,13 +1,42 @@
 package com.pashi44.patientservice.grpc;
 
+import org.hibernate.engine.spi.Managed;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
+
+import com.pashi44.billing.BillingRequest;
+import com.pashi44.billing.BillingResponse;
+import com.pashi44.billing.BillingServiceGrpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.Value;
 
 @Service
 public class BillingServiceGrpcClient {
-    
 
-    private final BillingServiceGrpcClient.BillingServiceBlockingStub billingServiceBlockingStub;
-    public BillingServiceGrpcClient(BillingServiceGrpcClient.BillingServiceBlockingStub billingServiceBlockingStub) {
-        this.billingServiceBlockingStub = billingServiceBlockingStub;
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(BillingServiceGrpcClient.class);
+
+    public BillingServiceGrpcClient(
+            @Value("${grpc.server.address}") String grpcServerAddress,
+            @Value("${grpc.server.port}") int grpcServerPort
+    ) {
+        logger.info("connecting to gRPC server");
+        logger.info("gRPC server address: {}", grpcServerAddress);
+        logger.info("gRPC server port: {}", grpcServerPort);
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(grpcServerAddress, grpcServerPort)
+                .usePlaintext()
+                .build();
+        blockingStub = BillingServiceGrpc.newBlockingStub(channel);
+
     }
+
+    public BillingResponse getBillingInfo(String ) {
+        logger.info("Received billing request: {}", request);
+        BillingResponse response = blockingStub.getBillingInfo(request);
+        logger.info("Received billing response: {}", response);
+        return response;
+    }
+
 }
